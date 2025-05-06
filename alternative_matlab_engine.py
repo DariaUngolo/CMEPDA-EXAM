7f067693d651efa620f55edbb2594cac487a1a3e
BrandoSpinelli <b.spinelli2@studenti.unipi.it>
2025-05-06 17:59:23 +0200

    correct way of connecting python and matlab

--- alternative_matlab_engine.py
+++ alternative_matlab_engine.py
@@ -29,7 +29,7 @@ def feature_extractor(folder_path, atlas_file, atlas_txt, output_csv_prefix):
 # Start MATLAB engine
     eng = matlab.engine.start_matlab()
 
-    eng.addpath(".")  # Add the current directory to MATLAB path
+    eng.addpath(r"C:\Users\brand\OneDrive\Desktop\CMEPDA-EXAM", nargout=0)  # Add the current directory to MATLAB path
 # Get the current MATLAB working directory
     current_folder=(eng.pwd())
 
@@ -55,7 +55,7 @@ def feature_extractor(folder_path, atlas_file, atlas_txt, output_csv_prefix):
 
 # Read the metadata CSV file that contains group information (e.g., diagnosis)
 
-    df_group = pd.read_csv("C:\\Users\\daria\\OneDrive\\Desktop\\ESAME\\AD_CTRL_metadata _AD.csv")
+    df_group = pd.read_csv(r"C:\Users\brand\OneDrive\Desktop\CMEPDA\progetto esame\data\AD_CTRL_metadata_AD.csv")
     df_group.sort_values(by=["ID"], inplace=True)   # Sort the dataframe by the "ID" column
     # Extract the diagnosis group labels (e.g., AD or CTRL) into a pandas Series
     group = df_group["DXGROUP"]
@@ -69,11 +69,11 @@ if __name__ == "__main__":
     """
 
      # Define the file paths for input data and output files
-    folder_path = r"C:\Users\daria\OneDrive\Desktop\ESAME\AD_s3\AD_s3"
-    atlas_file = r"C:\Users\daria\OneDrive\Desktop\ESAME\lpba40.spm5.avg152T1.gm.label.nii.gz"
-    atlas_txt = r"C:\Users\daria\OneDrive\Desktop\ESAME\lpba40_labelID.txt"
-    output_csv_prefix = r"C:\Users\daria\OneDrive\Desktop\ESAME\output"
-    metadata_csv = r"C:\Users\daria\OneDrive\Desktop\ESAME\AD_CTRL_metadata_AD.csv"
+    folder_path = r"C:\Users\brand\OneDrive\Desktop\CMEPDA\progetto esame\data\AD_s3\AD_s3"
+    atlas_file = r"C:\Users\brand\OneDrive\Desktop\CMEPDA\progetto esame\data\lpba40.spm5.avg152T1.gm.label.nii.gz"
+    atlas_txt = r"C:\Users\brand\OneDrive\Desktop\CMEPDA\progetto esame\data\lpba40_labelID.txt"
+    output_csv_prefix = r"C:\Users\brand\OneDrive\Desktop\output"
+    metadata_csv = r"C:\Users\brand\OneDrive\Desktop\CMEPDA\progetto esame\data\AD_CTRL_metadata_AD.csv"
 
     # Call the feature extraction function and obtain the results
     df_mean, df_std, group = feature_extractor(folder_path, atlas_file, atlas_txt, output_csv_prefix)
